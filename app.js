aa0369e5feb25ee201a737085ed437055dd13a40
Jeremy Black <jb@yodacom.com>
2016-11-30 06:39:51 -0700

    added a more detailed template for output
    Signed-off-by: Jeremy Black <jb@yodacom.com>

--- app.js
+++ app.js
@@ -9,10 +9,23 @@ var addItem = function(toDos, item) {
   toDos.items.push(item);
 };
 
+
 //render functions
 var renderToDos = function(toDos, element){
   var itemsHTML = toDos.items.map(function(item){
-    return "<li>" + item + "</li>";
+    return "<li>"
+       + item +
+       '<span class="shopping-item js-shopping-item"></span>' +
+    '<div class="shopping-item-controls">' +
+      '<button class="js-shopping-item-toggle">' +
+        '<span class="button-label">check</span>' +
+      '</button>' +
+      '<button class="js-shopping-item-delete">' +
+        '<span class="button-label">delete</span>' +
+      '</button>' +
+    '</div>' +
+  '</li>'
+       '</li>';
   });
 
   element.html(itemsHTML);
@@ -20,11 +33,9 @@ var renderToDos = function(toDos, element){
   console.log(toDos);
 };
 
-var toggleCrossout = function (){
-  $("button.shopping-item-toggle").click(function() {
-    $(this ).toggleClass( "shopping-item_checked");
+var toggleCrossout = $("button.shopping-item-toggle").click(function(event) {
+    $(event.target ).closest(".shopping-item-toggle").toggleClass( "shopping-item_checked");
   });
-};
 
 // Event listeners
 $("#js-shopping-list-form").submit(function(event){
@@ -35,5 +46,5 @@ $("#js-shopping-list-form").submit(function(event){
 
 $(".shopping-item-toggle").click(function(event){
   event.preventDefault();
-  event.toggleCrossout();
+  toggleCrossout();
 })
